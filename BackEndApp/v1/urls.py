26d55c95a6982109807d5ce6aecbef530cd00d00
Vrinda <vrindagupta6828@gmail.com>
2020-11-20 20:52:20 +0530

    login added

--- BackEndApp/v1/urls.py
+++ BackEndApp/v1/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import RegisterAPI
+from .views import RegisterAPI,LoginAPI
+from knox import views as knox_views
 
 urlpatterns = [
     path('generate/', views.generate, name='generate'),
     path('train/', views.train, name='train'),
     path('compile/', views.compile, name='compile'),
     path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
 ]
